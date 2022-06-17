b910c021d49799f276c7159d8a26f43b4a98f215
Mateo Martinez <51098519+MMAT30@users.noreply.github.com>
2022-06-17 13:40:15 -0500

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-tmux kill-server && cd ~/personalSite
+tmux kill-server
+cd ~/personalSite
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate && pip3 install -r requirements.txt
+source python3-virtualenv/bin/activate 
+pip3 install -r requirements.txt
 tmux new-session -d -s personalSite 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
