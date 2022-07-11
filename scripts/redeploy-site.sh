9c66c9468a8667c9b8e213769bb22f0de6c153f2
Mateo Martinez <mateo.martinez.spc@icloud.com>
2022-07-11 15:10:44 -0500

    updated redeploy script

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-systemctl stop myportfolio
-cd ~/personalSite
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yaml down
 docker compose -f docker-compose.prod.yaml up -d --build
